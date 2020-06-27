dff6ee3dbeaacaec59f3d4611141e150b66eee87
Elderly-AI <56202288+Elderly-AI@users.noreply.github.com>
2020-06-28 02:14:09 +0300

    docs

--- yamaps.py
+++ yamaps.py
@@ -1,26 +1,50 @@
+
 import aiohttp
 import asyncio
 
 
 class YaMaps:
-    def __init__(self, key):
+    """ Конструктор класса YaMaps
+        key(stirng) - ваш api ключ для yandex maps
+    """
+    def __init__(self, key: str) -> None:
         self.url = f'https://search-maps.yandex.ru/v1/?apikey={key}'
         self.session = None
 
-    def _open(self):
+    """ Protected метод для открытия сессии aiohttp
+        Лучше использовать async with
+    """
+    def _open(self) -> None:
         self.session = aiohttp.ClientSession()
 
-    async def _close(self):
+    """ Protected метод для закрытия сессии aiohttp,
+        Должен вызываться после завернения работы с YaMaps
+        Лучше использовать async with
+    """
+    async def _close(self) -> None:
         await self.session.close()
 
-    async def __aenter__(self):
+    """ Метод, определяющий, что происходит при вызове async with 
+        В данном случае происходит открытие сессии aiohttp    
+    """
+    async def __aenter__(self) -> object:
         self._open()
         return self
 
-    async def __aexit__(self, exception_type, exception_value, traceback):
+    """ Метод, определяющий, что происходит после выхода из тела async with 
+        В данном случае происходит закрытие сессии aiohttp    
+        Исключения на данном моменте не обрабатываются и пропускаются выше
+    """
+    async def __aexit__(self, exception_type, exception_value, traceback) -> None:
         await self._close()
 
-    async def request(self, **kwargs):
+    """ Метод, реализующий запрос к api yandex maps
+        kwargs(dict) - набор параметров, отсылаемых через get запрос
+        Где ключ - это имя параметра, значение - это значение параметра
+        Список всех ключей и возможных параметров и требуемый ответ от сервера 
+        можно посмотреть здесь https://tech.yandex.ru/maps/geosearch/doc/concepts/request-docpage/    
+    """
+    async def request(self, **kwargs: dict) -> object:
         request = "".join([f"&{key}={value}" for key, value in kwargs.items()])
         async with self.session.get(self.url + request) as resp:
             return await resp.json()
@@ -30,5 +54,6 @@ class YaMaps:
 async def test():
     async with YaMaps("12b9aefc-0d5e-49ae-bfe2-75ee6cb61816") as yamap:
         print(await yamap.request(text="Кофе", lang="ru_RU"))
+
 loop = asyncio.get_event_loop()
 result = loop.run_until_complete(test())
